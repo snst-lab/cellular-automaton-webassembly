d97372f36476262b8ffcfab6b78a70364d8c8add
snst-lab <snst.lab@gmail.com>
2019-07-11 23:00:51 +0900

    patch-0.1.7
    improve environment

---
+++ src/public/wasm/pkg/wasm_bg.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_cellularautomaton_free(a: number): void;
+export function __wbg_get_cellularautomaton_n(a: number): number;
+export function __wbg_set_cellularautomaton_n(a: number, b: number): void;
+export function __wbg_get_cellularautomaton_N(a: number): number;
+export function __wbg_set_cellularautomaton_N(a: number, b: number): void;
+export function __wbg_get_cellularautomaton_size_of_cell(a: number): number;
+export function __wbg_set_cellularautomaton_size_of_cell(a: number, b: number): void;
+export function cellularautomaton_new(): number;
+export function cellularautomaton_start(a: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbg_function_table: WebAssembly.Table;
